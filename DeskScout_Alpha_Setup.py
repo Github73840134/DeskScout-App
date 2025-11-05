27e05f2ca17e044e23f9bf5e2d7178b28a08453d
Github73840134 <35036025+Github73840134@users.noreply.github.com>
2025-11-04 19:03:26 -0500

    Update DeskScout_Alpha_Setup.py

--- DeskScout_Alpha_Setup.py
+++ DeskScout_Alpha_Setup.py
@@ -1,4 +1,4 @@
-import zipimport,zipfile,urllib.request as request,os,sys,json
+import zipimport,zipfile,urllib.request as request,os,sys,json,shutil,subprocess
 from tkinter import messagebox
 
 try:
@@ -26,6 +26,7 @@ layout = [
 	[sg.Text("Getting ready to install",key="status")]
 ]
 window = sg.Window("DeskScout Installer",layout,finalize=True,no_titlebar=True)
+window.refresh()
 try:
 	resp = request.urlopen("https://raw.githubusercontent.com/Github73840134/DeskScout-App/refs/heads/main/manifest.json")
 	path = json.loads(resp.read())[sys.platform]['alpha']
@@ -34,6 +35,8 @@ except Exception as e:
 	messagebox.showerror("DeskScout Installer",f"Unable to install DeskScout\n\n{str(e)}\nPhase: 3")
 	exit(0)
 window['status'].update("Downloading DeskScout")
+window.refresh()
+
 try:
 	resp = request.urlopen(f"https://raw.githubusercontent.com/Github73840134/DeskScout-App/refs/heads/main/{path}")
 	file = open(os.path.join(os.environ["temp"],"DeskScout Alpha Installer","app.zip"),'wb+')
@@ -44,7 +47,20 @@ except Exception as e:
 	messagebox.showerror("DeskScout Installer",f"Unable to install DeskScout\n\n{str(e)}\nPhase: 3")
 	exit(0)
 window['status'].update("Installing DeskScout")
+window.refresh()
 try:
-	os.mkdir(os.path.join(os.environ['HOMEDRIVE'],os.environ['HOMEPATH'],"DeskScout"))
-except IsADirectoryError:
-	messagebox.showinfo("DeskScout Installer","You already have DeskScout installed. If you want to update please do so via the app. Otherwise delete the app before installing")
+	os.mkdir(os.path.join(os.environ['HOMEDRIVE'],os.environ['HOMEPATH'],"DeskScout Alpha"))
+except FileExistsError:
+	messagebox.showinfo("DeskScout Installer","You already have DeskScout Alpha installed. If you want to update please do so via the app. If your app is corrupted delete the app before installing")
+	exit(0)
+try:
+	zip = zipfile.ZipFile(os.path.join(os.environ["temp"],"DeskScout Alpha Installer","app.zip"))
+	zip.extractall(os.path.join(os.environ['HOMEDRIVE'],os.environ['HOMEPATH'],"DeskScout Alpha"))
+	zip.close()
+except Exception as e:
+	messagebox.showerror("DeskScout Installer",f"Unable to install DeskScout\n\n{str(e)}\nPhase: 4")
+	exit(0)
+shutil.rmtree(os.path.join(os.environ["temp"],"DeskScout Alpha Installer"))
+window.close()
+subprocess.Popen(f"pyw \"{os.path.join(os.environ['HOMEDRIVE'],os.environ['HOMEPATH'],'DeskScout Alpha','app','DeskScout.pyw')}\"",start_new_session=True)
+print("DONE!")
